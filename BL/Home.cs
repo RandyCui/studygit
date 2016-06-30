c95c9a9f5e96f114948d5c0912b0e71be317a069
Randy Cui <randy@aa.com>
2016-06-30 09:38:31 +0800

    Add unit test

---
+++ BL/Home.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Home
+    {
+        public bool Test(string testStr)
+        {
+            if (string.IsNullOrEmpty(testStr))
+            {
+                return false;
+            }
+            if (testStr.Length > 5)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var bl = new BL.Home();
+            var testStr = string.Empty;
+            var result = bl.Test(testStr);
+            Assert.AreEqual(false, result, "Right for empty");
+            testStr = "asdadadadasda";
+            result = bl.Test(testStr);
+            Assert.AreEqual(false, result, "Right for > 5");
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var bl = new BL.Home();
+            var testStr = "as";
+            var result = bl.Test(testStr);
+            Assert.AreEqual(false, result, "Right for <=5");
+        }
+    }
+}
